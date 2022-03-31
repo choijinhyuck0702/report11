1da72464d5b52182495cf1730f3a01a1ece019fe
choijinhyuck0702 <79233908+choijinhyuck0702@users.noreply.github.com>
2022-03-31 15:50:49 +0900

    Add files via upload

--- 데이터베이스 구현-최진혁.sql
+++ 데이터베이스 구현-최진혁.sql
@@ -2,12 +2,14 @@ CREATE DATABASE BBS_db;
 use BBS_db;
 
 CREATE TABLE Border(
-	회원번호 INT not null,
+	회원번호 INT not null auto_increment,
     제목 VARCHAR(50) not null,
     내용 TEXT not null,
+    작성자 VARCHAR(10) not null,
+    비밀번호 VARCHAR(20) not null,
     primary key(회원번호)
 );
-
-INSERT INTO Border VALUES(1, '기본글', '기본글입니다.');
+drop table Border;
+INSERT INTO Border VALUES(1, '기본글', '기본글입니다.', '관리자', '1234');
 
 select * from Border;
